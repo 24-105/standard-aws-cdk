f0e52de427699b89c2e6015749bb854e3aad1242
tsujiryuma <r.0918.ryuma@icloud.com>
2024-09-04 00:57:18 +0900

    fix: husky validate

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,22 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'pref',
-        'test',
-        'build',
-        'ci',
-        'chore',
-        'revert',
-      ],
-    ],
-  },
 };
